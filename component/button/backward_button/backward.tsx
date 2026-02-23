a23a8fcef49fb159a5b68e99696559f689edf49a
Ashraf Khan <ashrfkhn97@gmail.com>
2026-02-23 10:09:32 +0530

    added back button

--- component/button/backward_button/backward.tsx
+++ component/button/backward_button/backward.tsx
@@ -1,8 +1,9 @@
 import { MoveLeft } from "lucide-react";
-
+import { useRouter } from "next/navigation";
 export default function BackwardButton({text}: {text: string}) {
+    const router = useRouter();
     return (
-        <button className="flex items-center gap-4 ml-1.5 mt-3 font-medium text-xl text-white">
+        <button className="flex items-center gap-4 ml-1.5 mt-3 font-medium text-xl text-white" onClick={() => router.back()}>
             <MoveLeft size={24} />
             <h2>{text}</h2>
         </button>
