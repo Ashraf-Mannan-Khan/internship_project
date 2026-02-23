7d695718aa81db27bd893473ba0c9e919cd6f188
Ashraf Khan <ashrfkhn97@gmail.com>
2026-02-23 10:30:05 +0530

    added  active class to menu section

--- component/menu/menu.tsx
+++ component/menu/menu.tsx
@@ -1,10 +1,13 @@
+"use client";
 import styles from "./menu.module.css"; 
 import { Home, User, CalendarDays, ClipboardClock } from 'lucide-react';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function Menu() {
+    const pathname = usePathname();
     return (
         <div className={styles.menuContainer}>
-            <Link href="/home" className={styles.menuLink}><div className={styles.menuItem}>
+            <Link href="/home" className={`${styles.menuLink} ${pathname === '/home' ? styles.active : ''}`}><div className={styles.menuItem}>
                 <Home size={24} className={styles.menuIcon} />
                 <span className={styles.menuText}>Home</span>
             </div>
